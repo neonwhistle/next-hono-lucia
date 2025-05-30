80e3c4215aacf6d41f589322c1a6d4fff68db541
neonwhistle <162157495+neonwhistle@users.noreply.github.com>
2025-05-29 21:49:18 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import { serverEnvs } from '@/env/server';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    output: serverEnvs.STANDALONE === 1 ? 'standalone' : undefined,
+    output: serverEnvs.STANDALONE === '1' ? 'standalone' : undefined,
     reactStrictMode: true,
     serverExternalPackages: ['@node-rs/argon2'],
 };
